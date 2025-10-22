dd1d4b031ba185d10a1cd44b3abf8618e11ffce4
Gyovanna16 <jaidegyovanna@gmail.com>
2025-10-22 19:29:42 +0000

    22/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Header } from "./components/Header";
 import { ProductList } from "./components/ProductList";
-import { Cart } from "./components/Cart";
+import { Header } from "./components/Header";
 import { Route, Routes } from "react-router";
+import { Cart } from "./components/Cart";
 import { CartProvider } from "./context/CartContext";
+import { Login } from "./components/Login";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
+
   return (
-    // React Fragment
     <>
+      <ToastContainer />
       <CartProvider>
         <Header />
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/cart" element={<Cart />} />
-          {/* Add more routes as needed */}
+          <Route path="/signin" element={<Login value="signin" />} />
+          <Route path="/register" element={<Login value="register" />} />
         </Routes>
       </CartProvider>
     </>
